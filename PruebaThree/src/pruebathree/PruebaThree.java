964d05ac46b5a6352d538cc9c7fadb081bd9e297
VictorGarduno <ve15421@innovaccion.mx>
2021-04-21 12:29:31 -0600

    Actualizacion 2.0

--- PruebaThree/src/pruebathree/PruebaThree.java
+++ PruebaThree/src/pruebathree/PruebaThree.java
@@ -9,6 +9,7 @@ public class PruebaThree {
         int b = 2;
         System.out.println(a+b);
         System.out.println("En todo funcionan los cambios");
+        System.out.println("En todo funcionan los cambios");
     }
     
 }

1a2006f4077d66c7b1636648c0b48af1d956b82d
andreselotakugamer@hotmail.com <>
2021-04-22 14:01:19 -0600

    alex version 2.0 maybe

---
+++ PruebaThree/src/pruebathree/alex.java
@@ -0,0 +1,22 @@
+package pruebathree;
+
+public class alex {
+    private int a;
+    private int b;
+    public alex(int a, int b){
+         this.a = a;
+         this.b = b;
+        
+    }
+    public int get(){
+        return a;
+        
+    }
+    public int getb(){
+        return b;
+    }
+    public int suma(){
+        int result = a+b;
+        return result;
+    }
+}

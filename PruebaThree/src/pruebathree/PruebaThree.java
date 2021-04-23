bd4b1604a7a51d8612029360d70d51d0257ad24d
Raymundo <Raymundo@DESKTOP-BAC2TE1>
2021-04-22 22:59:47 -0600

    Ejercicio_poo prueba

--- PruebaThree/src/pruebathree/PruebaThree.java
+++ PruebaThree/src/pruebathree/PruebaThree.java
@@ -2,14 +2,42 @@
 package pruebathree;
 
 public class PruebaThree {
+    int bb;
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo GitHub, Soy Andrew");
         int a = 1;
         int b = 2;
-        System.out.println(a+b);
+        double x = 3;
+        System.out.println(a+b+x);
         System.out.println("En todo funcionan los cambios");
-        System.out.println("En todo funcionan los cambios");
+        System.out.println("Todos los cambios funcionan");
+        
+        System.out.println("Prueba hecha por Raymundo Baca");
+        System.out.println("Hola mundo");
+        
+        
+        PruebaThree abx=new PruebaThree();
+        abx.resta();
+        boolean respuesta=true;
+        automovil aut=new automovil();
+        aut.color=rojo;
+        aut.motor=true;
+        
     }
-    
+    public void resta(){
+        int r=35;
+        int d=12;
+        int resultado=(r-d);
+        
+        System.out.println(resultado);
+                
+    }
+    public void motor (int llantas){
+        System.out.println("g222");
+        
+    }
+    public void motor (int ){
+        System.ount
+    }
 }
---
+++ PruebaThree/src/pruebathree/automovil.java
@@ -0,0 +1,16 @@
+
+package pruebathree;
+
+public class automovil {
+    boolean motor;
+    private int pedales;
+    boolean volante;
+    protected String color;
+    int llantas;
+    
+    
+    public void setP(int pedales){
+        this.pedales=pedales;
+        
+    }
+}

b8f72300afc5bc8c8a06caf81ec19016d498b69c
andreselotakugamer@hotmail.com <>
2021-04-22 22:59:19 -0600

    Voy a actualizarlo

---
+++ PruebaThree/src/pruebathree/Auto.java
@@ -0,0 +1,26 @@
+package pruebathree;
+public class Auto {
+    
+    boolean motor;
+    private int pedales;
+    boolean volante;
+    protected String color;
+    private int llantas;
+    
+    public void motor(int llantas){
+        System.out.println("g222rg");
+    }
+    public String a (String color){
+        String a=color;
+        return a;
+    }
+    
+            
+    
+    public void motor(){
+        System.out.println("grg");
+    }
+            
+ 
+    
+}
--- PruebaThree/src/pruebathree/PruebaThree.java
+++ PruebaThree/src/pruebathree/PruebaThree.java
@@ -2,14 +2,49 @@
 package pruebathree;
 
 public class PruebaThree {
+    
+    int bbb;
+    
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo GitHub, Soy Andrew");
         int a = 1;
         int b = 2;
-        System.out.println(a+b);
-        System.out.println("En todo funcionan los cambios");
-        System.out.println("En todo funcionan los cambios");
+        double c = 3;
+        System.out.println(a+b+c);
+        
+        PruebaThree abc = new PruebaThree();
+        abc.suma();
+        boolean respuesta=true;
+        Auto at = new Auto();
+        at.color="Rojo";
+        at.motor=true;
+        
+        Auto deAlex = new Auto();
+        deAlex.color= "Azul";
+        deAlex.volante=true;
+        
+        System.out.println(deAlex.a(deAlex.color));
+        
+         System.out.println(at.a(at.color));
+        
+  
     }
+    
+    
+    
+    
+    public void suma(){
+    int d=3,h=5;
+    int resultado = d*h/2+6;
+    System.out.println(resultado);
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
